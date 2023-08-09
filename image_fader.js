0119dde1857cf3f6dd549442a298e99bedbddb31
Aditya Kishan Ankaraboyana <kishanankara@gmail.com>
2023-08-09 10:38:39 -0700

    change to react app

--- image_fader.js
+++
@@ -1,8 +0,0 @@
-    window.onorientationchange = function() {
-        var orientation = window.orientation;
-            switch(orientation) {
-                case 0:
-                case 90:
-                case -90: window.location.reload();
-                break; }
-    };
