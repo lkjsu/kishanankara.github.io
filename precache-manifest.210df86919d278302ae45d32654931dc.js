9aa2d66d98df6d9d2bbc12183b0d5d42762f0732
Aditya Kishan Ankaraboyana <kishanankara@gmail.com>
2023-08-24 19:27:31 -0700

    Updates

---
+++ precache-manifest.210df86919d278302ae45d32654931dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc4df63798c894d8dddf",
+    "url": "/kishanankara.github.io/static/css/main.69dda8d1.chunk.css"
+  },
+  {
+    "revision": "dc4df63798c894d8dddf",
+    "url": "/kishanankara.github.io/static/js/main.dc4df637.chunk.js"
+  },
+  {
+    "revision": "b5dec1235d8d1d118e4f",
+    "url": "/kishanankara.github.io/static/js/1.b5dec123.chunk.js"
+  },
+  {
+    "revision": "2b75a149fd2ffd855e54",
+    "url": "/kishanankara.github.io/static/css/2.44eb7752.chunk.css"
+  },
+  {
+    "revision": "2b75a149fd2ffd855e54",
+    "url": "/kishanankara.github.io/static/js/2.2b75a149.chunk.js"
+  },
+  {
+    "revision": "659b29ea70a72d11ad53",
+    "url": "/kishanankara.github.io/static/js/runtime~main.659b29ea.js"
+  },
+  {
+    "revision": "425c433894e29b868009e6d57426e928",
+    "url": "/kishanankara.github.io/static/media/ND.425c4338.png"
+  },
+  {
+    "revision": "a4a60617632b9f8df4d663c6f5c464ec",
+    "url": "/kishanankara.github.io/index.html"
+  }
+];
