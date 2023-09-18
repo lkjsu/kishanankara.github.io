57f875887acfe3478815acf3d0a959906f82ca2b
Aditya Kishan Ankaraboyana <kishanankara@gmail.com>
2023-09-17 20:40:23 -0700

    change nav colors

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
 
-function setColor(date) {
+function setColor() {
     if(new Date().getHours() >= 18 || new Date().getHours() <= 6) {
         return "#183D3D";
     } else {
@@ -13,19 +13,13 @@ class Main extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            bgColor: setColor(new Date().toLocaleTimeString('en-GB'))
+            bgColor: setColor()
         };
         this.colors = ['#183D3D','#A8DF8E'];
         this.buttonNames = ['Lights OFF','Lights ON']
     }
 
     componentDidMount() {
-        // document.addEventListener('scroll', () => {
-        //     // console.log(window.scrollY);
-        //     var value = window.scrollY;
-        //     console.log(Math.round(Math.abs(value / 2)) % this.colors.length)
-        //     this.setState({bgColor:this.colors[Math.round(Math.abs(value / 4)) % this.colors.length]});
-        // });
     }
 
     handleClick(color) {
@@ -36,12 +30,6 @@ class Main extends React.Component {
     return (
             <div className="App-header" style={{ backgroundColor : this.state.bgColor}}>
             <p className="App-text" style={{color : this.state.bgColor === "#183D3D"? "#EEEEEE" :"#016A70"}}>Aditya Kishan Ankaraboyana</p>
-            {/* <div>
-            {
-                this.colors.map((c, index) => 
-                <button key={index} style={{backgroundColor: c, color: c === '#183D3D'? "white": "black"}} onClick={() => {this.handleClick(c)}}>{this.buttonNames[index]}</button>)
-            }
-            </div> */}
             </div>
             );
         }
--- src/Navigation.js
+++ src/Navigation.js
@@ -2,10 +2,26 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import Resume from './AKAResume2023.pdf';
 
+function getClassNameForNav() {
+    if(new Date().getHours() >= 18 || new Date().getHours() <= 6) {
+        return "nav-item-theme-dark";
+    } else {
+        return "nav-item-theme-light";
+    }
+}
 
-function setColor(date) {
+
+function setColor() {
     if(new Date().getHours() >= 18 || new Date().getHours() <= 6) {
         return "#183D3D";
+    } else {
+        return "#A8DF8E";
+    }
+}
+
+function setHoverColor() {
+    if(new Date().getHours() >= 18 || new Date().getHours() <= 6) {
+        return "#fff";
     } else {
         return "#A8DF8E";
     }
@@ -16,12 +32,12 @@ class Nav extends React.Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-success sticky">
-                <div className="navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
-                        <NavLink className="nav-item nav-link" to="/">Home</NavLink>
-                        <NavLink className="nav-item nav-link" to="/Experience">Experience</NavLink>
-                        <a href={Resume} className="nav-item nav-link">Resume</a>
+                <nav className="navbar" style={{backgroundColor: setColor()}}>
+                <div  id="navbarNavAltMarkup">
+                    <div>
+                        <NavLink className={getClassNameForNav()} hover={{backgroundColor: setHoverColor()}} to="/">Home</NavLink>
+                        <NavLink className={getClassNameForNav()} hover={{backgroundColor: setHoverColor()}} to="/Experience">Experience</NavLink>
+                        <a href={Resume} className={getClassNameForNav()}>Resume</a>
                     </div>
                 </div>
                 </nav>
