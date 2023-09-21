14310f50de984857eef7604d45fa5498876450ec
Aditya Kishan Ankaraboyana <kishanankara@gmail.com>
2023-09-20 22:25:31 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.700c82bda8e755f9999294093d96c36b.js"
+  "/precache-manifest.76c5805a813b45147ba9c2bf64c32b08.js"
 );
 
 workbox.clientsClaim();
