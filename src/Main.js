a76b6d2b7998b00a991b03a36d466460aa278dde
Aditya Kishan Ankaraboyana <kishanankara@gmail.com>
2023-10-07 17:25:04 -0700

    fix typo

--- src/Main.js
+++ src/Main.js
@@ -33,7 +33,7 @@ class Main extends React.Component {
             <div className="b" style={{color : this.state.bgColor === "#183D3D"? "#EEEEEE" :"#016A70"}}>Aditya Kishan Ankaraboyana</div>
             </div>
             {/* <div class="u" style={{color : this.state.bgColor === "#183D3D"? "#EEEEEE" :"#016A70"}}>Ankaraboyana</div> */}
-            <div class="n" style={{color : this.state.bgColor === "#183D3D"? "#EEEEEE" :"#016A70"}}>software engineer building great expereiences.</div>
+            <div class="n" style={{color : this.state.bgColor === "#183D3D"? "#EEEEEE" :"#016A70"}}>software engineer building great experiences.</div>
             {/* <p1></p1> */}
             {/* <div class="letter-image">
             <div class="animated-mail">
