a6b65d0e347291f5ef24289f1e13321cc4ad9bf1
Aditya Kishan Ankaraboyana <kishanankara@gmail.com>
2023-10-15 17:22:41 -0700

    add new Resume

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import Resume from './AKAResume2023_latex2.pdf';
+import Resume from './AKAResume2023_latex3.pdf';
 
 function getClassNameForNav() {
     if(new Date().getHours() >= 18 || new Date().getHours() <= 6) {
