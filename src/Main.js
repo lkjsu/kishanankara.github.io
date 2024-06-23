d4171bd890d99f8df2ad225c852b53d7212f78d7
Aditya Kishan Ankaraboyana <kishanankara@gmail.com>
2024-06-23 16:38:17 -0700

    add default contrast

--- src/Main.js
+++ src/Main.js
@@ -8,11 +8,11 @@ class Main extends React.Component {
     return (
             <div className="App-header" style={{ backgroundColor : bgColor}}>
                 <div className="intro" style={{ backgroundColor : bgColor}}>
-                    <div className="fade-in-text1" style={{color : bgColor === "#053B50"? "#EEEEEE" :"#016A70"}}>Hello, </div>
-                    <div className="fade-in-text2" style={{color : bgColor === "#053B50"? "#EEEEEE" :"#016A70"}}>I </div>
-                    <div className="fade-in-text3" style={{color : bgColor === "#053B50"? "#EEEEEE" :"#016A70"}}>am</div>
+                    <div className="fade-in-text1" style={{color : bgColor === "#183D3D"? "#EEEEEE" :"#016A70"}}>Hello, </div>
+                    <div className="fade-in-text2" style={{color : bgColor === "#183D3D"? "#EEEEEE" :"#016A70"}}>I </div>
+                    <div className="fade-in-text3" style={{color : bgColor === "#183D3D"? "#EEEEEE" :"#016A70"}}>am</div>
                 </div>
-                <div className="fade-in-text4" style={{color : bgColor === "#053B50"? "#EEEEEE" :"#016A70"}}>Aditya Kishan Ankaraboyana.</div>
+                <div className="fade-in-text4" style={{color : bgColor === "#183D3D"? "#EEEEEE" :"#016A70"}}>Aditya Kishan Ankaraboyana.</div>
                 {/* <div class="fade-in-text5" style={{color : this.state.bgColor === "#053B50"? "#EEEEEE" :"#016A70"}}>software engineer with a keen interest in Backend Development.</div> */}
             </div>
             );
