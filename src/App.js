2b086968b44ab8287c165eda16905ceb6841e016
Aditya Kishan Ankaraboyana <kishanankara@gmail.com>
2024-07-13 06:08:07 +0530

    fix bug related to date time

--- src/App.js
+++ src/App.js
@@ -17,39 +17,67 @@ class App extends React.Component {
         };
     }
 
+    convertTimeToUnix(timeInString) {
+        // console.log(timeInString)
+        var vals = timeInString.split(' ');
+        // console.log("vals" ,vals , new Date().getMonth()+1)
+        var date = vals[0].split("/")
+        // console.log("Date inside", date)
+        var time = vals[1].split(":")
+        if (vals[2] === 'PM') {
+            time[0] = String(parseInt(time[0]) + 12);
+        }
+        // console.log("Date now" , date, time)
+        var unixTimeStamp = new Date(Number(date[0]), Number(date[1])-1, Number(date[2]), Number(time[0]), Number(time[1]), Number(time[2]))
+        // console.log(unixTimeStamp - new Date().getTime())
+        // console.log("Full year", unixTimeStamp.getFullYear())
+        // console.log("Unix timestamp", new Date(unixTimeStamp.getTime()))
+        // console.log("Year", new Date(unixTimeStamp * 1000).getFullYear())
+        return unixTimeStamp.getTime()
+    }
+
     componentDidMount() {
         if ('geolocation' in navigator) {
             navigator.geolocation.getCurrentPosition(async (position) => {
                 let obj = `lat=${position.coords.latitude}&lng=${position.coords.longitude}`
+                // console.log(obj)
                 let response = await fetch("https://api.sunrisesunset.io/json?" + obj);
                 // console.log("https://api.sunrisesunset.io/json?" + obj)
                 let resp_obj = await response.json();
                 let result = resp_obj.results
                 const date = new Date()
-                const ymd = date.getFullYear() + '/' + date.getMonth() + '/' + date.getDay()
+                // console.log("Date YMD", date.getFullYear)
+                const ymd = date.getFullYear() + '/' + String(Number(date.getMonth())+1) + '/' + date.getDate()
+                // console.log("ymd value", ymd)
                 const sunrise = ymd + " " + result.sunrise
                 const sunset = ymd + " " + result.sunset
+                var unixsunrise = this.convertTimeToUnix(sunrise);
+                var unixsunset = this.convertTimeToUnix(sunset);
+                // console.log(result.sunrise, result.sunset )
                 // console.log(result)
                 // console.log(sunrise, sunset)
-                const unixsunrise = new Date(sunrise).getTime()
-                const unixsunset = new Date(sunset).getTime()
-
-                // console.log(new Date().getTime() >= unixsunrise, new Date().getTime() <= unixsunset)
+                // const unixsunrise = new Date(sunrise).getTime()
+                // const unixsunset = new Date(sunset).getTime()
+                // console.log(compareSunrise, unixsunrise, compareSunset, unixsunset);
+                // console.log(unixsunset - unixsunrise , new Date().getTime() - unixsunrise, unixsunset - new Date().getTime())
+                // console.log(new Date().getTime() >= unixsunrise && new Date().getTime() <= unixsunset)
                 // console.log(unixsunrise, unixsunset)
                 // console.log(new Date().getTime() >= unixsunrise,new Date().getTime() <= unixsunset)
                 // console.log("Geolocation allowed!")
+                // console.log(unixsunset - new Date().getTime(), unixsunrise - new Date().getTime());
+
                 if(new Date().getTime() >= unixsunrise && new Date().getTime() <= unixsunset) {
                     this.setState({
                         bgColor: "#A8DF8E",
                         hoverColor: "#A8DF8E",
                         className: "nav-item-theme-light"
-                    })
+                    });
                 } else {
                     this.setState({
                         bgColor: "#183D3D",
                         hoverColor: "#fff",
                         className: "nav-item-theme-dark"
-                    })
+                    });
                 }
 
 
@@ -60,13 +88,13 @@ class App extends React.Component {
                         bgColor: "#183D3D",
                         hoverColor: "#fff",
                         className: "nav-item-theme-dark"
-                    })
+                    });
                 } else {
                     this.setState({
                         bgColor: "#A8DF8E",
                         hoverColor: "#A8DF8E",
                         className: "nav-item-theme-light"
-                    })
+                    });
                 }
                 this.setState({ error: error.message });
             });
@@ -78,13 +106,13 @@ class App extends React.Component {
                     bgColor: "#183D3D",
                     hoverColor: "#fff",
                     className: "nav-item-theme-dark"
-                })
+                });
             } else {
                 this.setState({
                     bgColor: "#A8DF8E",
                     hoverColor: "#A8DF8E",
                     className: "nav-item-theme-light"
-                })
+                });
             }
             this.setState({error: "Browser doesn't support geolocation, switching back to the default."});
         }
